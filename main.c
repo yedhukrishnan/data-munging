d66ebaba2f298ee74fde8f435d5c3716062d94d8
Yedhu Krishnan <yedhu_krishnan@yahoo.com>
2015-02-28 16:50:00 +0530

    Finish first version of first part

--- main.c
+++ main.c
@@ -5,14 +5,20 @@
 
 int main()
 {
-  int i;
+  int i, temperature_spread, day;
   char **lines = read_lines_from_file("weather.dat");
   struct weather *weather_data = get_weather_data(lines);
-  
-  for(i = 0; i < MAX_DAY_COUNT; i++)
-    {
-      printf("%d\n", weather_data[i].day);
-    }
+
+  temperature_spread = weather_data[0].max_temperature - weather_data[0].min_temperature;
+  day = weather_data[0].day;
+  for(i = 1; i <= MAX_DAY_COUNT; i++)
+    if(temperature_spread > (weather_data[i].max_temperature - weather_data[i].min_temperature))
+      {
+        temperature_spread = weather_data[i].max_temperature - weather_data[i].min_temperature;
+        day = weather_data[i].day;
+      }
+
+  printf("Day with minimum temperature spread: %d\n", day);
 
   return 0;
 }
